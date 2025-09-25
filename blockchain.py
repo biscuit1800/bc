530e9d2df8bff4cc013db3601955334953683562
biscuit1800 <perekora71@gmail.com>
2025-09-25 19:16:41 +0300

    first commit

---
+++ blockchain.py
@@ -0,0 +1,21 @@
+class Blockchain():
+    def __init__(self):
+        pass
+
+    def new_block(self):
+        # creation of new block
+        pass
+
+    def new_transaction(self):
+        # creation of new transaction
+        pass
+
+    @staticmethod
+    def hash(block):
+        # hashes the block
+        pass
+
+    @property
+    def last_block(self):
+        # returns last block in chain
+        pass
